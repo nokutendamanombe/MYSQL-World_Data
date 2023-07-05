75780508c38ce7dfb49c3f6123df9722a79e35e1
Nokutenda Manombe <138527119+nokutendamanombe@users.noreply.github.com>
2023-07-05 13:47:02 +0400

    World Project Query Script.sql

--- World Project Query Script.sql
+++ World Project Query Script.sql
@@ -7,7 +7,43 @@ FROM
     join countrylanguage on country.Code = countrylanguage.CountryCode
 WHERE
     country.Region LIKE '%Southeast Asia%'
+
  
+/*Retrieve the names of all cities in the "city" table*/
+select city.name From city 
+
+/*Find the countries in the "country" table that have a population greater than 100 million.*/
+select country.name from country 
+where country.population > 100000000 
+
+/*Get the official languages and their percentages for a specific country from the "countrylanguage" table.*/
+select Language , IsOfficial, Percentage
+ from countrylanguage
+ where IsOfficial= True 
+ 
+ /*Retrieve the name of each city along with its corresponding country name.*/
+ select city.Name, country.Name
+ from city 
+ join country on city.countryCode = country.code 
+ 
+ /*Find the countries in a specific continent from the "country" table and display the cities within those countries from the "city" table.*/
+ select country.name , city.name from country 
+ join city on country.Code = city.countrycode 
+ where country.Continent like '%Africa%'
+ 
+ /*Find the top 5 countries with the highest life expectancy from the "country" table.*/
+ select country.LifeExpectancy from country 
+ Order by LifeExpectancy desc 
+ 
+ /*Calculate the total population for each continent from the "country" table.*/
+SELECT Continent, SUM(Population) AS TotalPopulation
+FROM country
+Group BY Continent;
+
+/*Find the average GNP (Gross National Product) for each region from the "country" table.*/
+SELECT region, AVG(GNP) AS AvgGNP
+FROM country
+GROUP BY region
 
 
  /* Which is the most densely populated country in the world?*/
